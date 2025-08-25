b0274408c181de75d6dde5995c5069bf7e7fed15
Matheus-Costa-26 <matheuscost256@gmail.com>
2025-08-25 19:40:16 -0400

    Create questao_10.c

---
+++ Lista_Exercicios_1_Ponteiros/questao_10.c
@@ -0,0 +1,10 @@
+2. O que o operador * (asterisco) faz quando usado na frente de uma variável do 
+tipo ponteiro que já foi inicializada?
+a) Declara um novo ponteiro.
+b) Retorna o endereço de memória do ponteiro.
+c) Realiza uma operação de multiplicação.
+d) Acessa (dereferencia) o valor armazenado no endereço de memória para o qual 
+o ponteiro aponta.
+
+Resposta: d) Acessa (dereferencia) o valor armazenado no endereço de memória para o qual 
+o ponteiro aponta.
