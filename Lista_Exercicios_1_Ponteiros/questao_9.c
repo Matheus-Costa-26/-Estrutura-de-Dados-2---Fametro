fdd5ce9fd060fb270e25bfb6cf97a4d01ed37566
Matheus-Costa-26 <matheuscost256@gmail.com>
2025-08-25 19:38:31 -0400

    Create questao_9.c

---
+++ Lista_Exercicios_1_Ponteiros/questao_9.c
@@ -0,0 +1,7 @@
+1. Qual das seguintes afirmações sobre ponteiros em C é VERDADEIRA?
+a) Um ponteiro armazena o valor de uma variável.
+b) Um ponteiro armazena o endereço de memória de uma variável.
+c) A declaração int *p; aloca memória para um inteiro.
+d) O operador & é usado para acessar o valor apontado por um ponteiro.
+
+reposta: b) Um ponteiro armazena o endereço de memória de uma variável.
